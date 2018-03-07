b24284b0c5653537ac9b6b2dbecd4df7be3790fc
Marco Romero - note do trabalho <mmcromero@gmail.com>
2018-03-07 17:47:14 -0300

    adicionando pluging de vibração

--- www/js/app.js
+++ www/js/app.js
@@ -14,4 +14,33 @@
 
   })
 
+var onShake = function () {
+  // Fired when a shake is detected 
+  alert("balançou.... kkkk");
+};
+ 
+var onError = function () {
+  // Fired when there is an accelerometer error (optional) 
+  alert("deu ruim...;(");
+};
+ 
+// Start watching for shake gestures and call "onShake" 
+// with a shake sensitivity of 40 (optional, default 30) 
 
+
+// Stop watching for shake gestures 
+//shake.stopWatch();
+
+
+function onLoad() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+}
+
+// device APIs are available
+//
+function onDeviceReady() {
+    // Now safe to use device APIs
+    alert("1");
+	shake.startWatch(onShake, 30);
+	alert("2");
+}
